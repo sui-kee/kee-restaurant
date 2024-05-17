6922e488b2d2706f0f405e6aa4b77ff4e5dd7fd3
hyenaManager <zodiacjelly9@gmail.com>
2024-05-17 21:06:25 +0800

    modify:

--- components/component/about-us.tsx
+++ components/component/about-us.tsx
@@ -42,7 +42,7 @@ export function AboutUs() {
                 <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
                   KEE RESTAURANT
                 </h1>
-                <p className="max-w-[600px] text-gray-500 md:text-xl dark:text-gray-400">
+                <p className="max-w-[600px] text-white bg-[rgba(0,0,0,0.5)] p-2 rounded-xl md:text-xl dark:text-gray-400">
                   Serving up delicious, locally-sourced cuisine in the heart of
                   the city. Experience the flavors of our community.
                 </p>
@@ -50,15 +50,15 @@ export function AboutUs() {
               <div className="flex flex-col gap-2 min-[400px]:flex-row">
                 <Link
                   className="inline-flex h-10 items-center justify-center rounded-md bg-gray-900 px-8 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
-                  href="#"
+                  href="/menu"
                 >
                   View Menu
                 </Link>
                 <Link
                   className="inline-flex h-10 items-center justify-center rounded-md border border-gray-200 bg-white px-8 text-sm font-medium shadow-sm transition-colors hover:bg-gray-100 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50  dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300 dark:border-gray-800"
-                  href="#"
+                  href="/reservation"
                 >
-                  Book a Table
+                  Make a reservation
                 </Link>
               </div>
             </div>
@@ -83,10 +83,21 @@ export function AboutUs() {
                 Visit us at our cozy location in the heart of the city, just a
                 short walk from the metro station.
               </p>
+              <Image
+                alt="Kitchen"
+                className="rounded-lg object-cover"
+                height="600"
+                src={"/location.jpg"}
+                style={{
+                  aspectRatio: "800/600",
+                  objectFit: "cover",
+                }}
+                width="800"
+              />
             </div>
             <div className="rounded-lg overflow-hidden" />
           </div>
-          <Location />
+          {/* <Location /> */}
         </div>
       </section>
       <section className="w-full py-12 md:py-24 lg:py-32">
