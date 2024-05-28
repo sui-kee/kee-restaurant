c6bec0664d126360eb607eacc88a01bc1eb69782
hyenaManager <zodiacjelly9@gmail.com>
2024-05-28 18:56:49 +0800

    change font style and logo

--- components/about-us/location.tsx
+++ components/about-us/location.tsx
@@ -22,9 +22,29 @@ const Location = () => {
   }, []);
 
   return (
-    <div>
-      <h1>Welcome to Our Restaurant</h1>
-      <p>Find us at:</p>
+    <div className=" flex flex-col gap-3">
+      <div className=" flex gap-2 items-center justify-start uppercase text-sm sm:text-3xl font-bold">
+        <h1>we are always on the same ground</h1>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="size-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+          />
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
+          />
+        </svg>
+      </div>
       <Map userLocation={userLocation} />
     </div>
   );
